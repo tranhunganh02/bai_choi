dd46f87deeda154de6da25e063f4d3da29730836
huyxoann <huytn.21it@vku.udn.vn>
2023-10-08 16:55:56 +0700

    Hoan thanh Main Menu, sửa fonts Aviano

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:bai_choi/utils/routes.dart' as route;
 import 'package:bai_choi/constaint.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -18,60 +19,94 @@ class _HomeScreenState extends State<HomeScreen> {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
               image: DecorationImage(
-            image: AssetImage("assets/img/bg_home.png"),
-            fit: BoxFit.cover,
-          )),
+                  image: const AssetImage("assets/img/bg_home_1.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                      const Color(0xFFB22A21).withOpacity(0.30),
+                      BlendMode.color))),
           child: Body(height, context),
         ),
-        floatingActionButton: optional_acction(context));
+        floatingActionButton: optional_action(context));
   }
 
   // ignore: non_constant_identifier_names
-  Padding Body(double height, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
-      child: Center(
-        child: SizedBox(
-          height: height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Title_game(),
-              Play_button(context),
-            ],
+  Row Body(double height, BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Container()),
+        Expanded(
+            child: Container(
+          color: Theme.of(context).colorScheme.primary,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Bài Chòi",
+                        style: TextStyle(
+                            fontFamily: "DelaGothicOne",
+                            fontSize: 30,
+                            color: const Color(0xFFFDB813),
+                            shadows: [
+                              Shadow(
+                                blurRadius: 4,
+                                color: Colors.black.withOpacity(0.25),
+                                offset: const Offset(3, 3),
+                              )
+                            ])),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16),
+                      child: Text(
+                        "Mang giá trị dân gian đến với mọi người",
+                        style: TextStyle(
+                            fontFamily: "Aviano",
+                            fontSize: 7,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+                Play_button(context)
+              ],
+            ),
           ),
-        ),
-      ),
+        )),
+        Expanded(child: Container())
+      ],
     );
   }
+
+  // Text titleGame(BuildContext context) {
+  //   return const Text("Bài Chòi",
+  //       style: TextStyle(
+  //           fontFamily: "DelaGothicOne",
+  //           fontSize: 30,
+  //           color: Color(0xFFFDB813)));
+  // }
 
   // ignore: non_constant_identifier_names
   GestureDetector Play_button(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.pushNamed(context, route.roomScreen),
       child: const SizedBox(
-          height: 80, width: 100, child: Center(child: TextCustom(fontFamily: 'DelaGothicOne', content: 'Bắt đầu', sizeText: 20, colorText: Colors.white))),
-    );
-  }
-
-  Container Title_game() {
-    return Container(
-      height: 100,
-      //color: Colors.lightBlue,
-      child: const ListTile(
-
-          // title: Text("Bài chòi",style: TextStyle(fontFamily: "DelaGothicOne", fontSize: 40, fontWeight: FontWeight.w900)),
-          // subtitle: Text("Mang giá trị dân gian đến với mọi người"),
-
-          ),
+          height: 80,
+          width: 100,
+          child: Center(
+              child: TextCustom(
+                  fontFamily: 'DelaGothicOne',
+                  content: 'Bắt đầu',
+                  sizeText: 20,
+                  colorText: Colors.white))),
     );
   }
 
   // ignore: non_constant_identifier_names
-  SpeedDial optional_acction(BuildContext context) {
+  SpeedDial optional_action(BuildContext context) {
     return SpeedDial(
       backgroundColor: Theme.of(context).colorScheme.secondary,
       animatedIcon: AnimatedIcons.menu_close,
--- lib/screens/introduction_game/components/list_card_1.dart
+++ lib/screens/introduction_game/components/list_card_1.dart
@@ -9,11 +9,11 @@ SingleChildScrollView ListCard() {
   return SingleChildScrollView(
     padding: EdgeInsets.all(15),
     child: Column(children: [
-      const TitleListCard(title: "Phò Sách"),
+      const TitleListCard(title: "Pho Sách"),
       type_card(listCardSach,  listCardSach.length,),
-      const TitleListCard(title: "Phò Văn"),
+      const TitleListCard(title: "Pho Văn"),
       type_card(listCardVan, listCardVan.length,),
-      const TitleListCard(title: "Phò Vạn"),
+      const TitleListCard(title: "Pho Vạn"),
       type_card(listCardVawn, listCardVawn.length),
     ]),
   );
