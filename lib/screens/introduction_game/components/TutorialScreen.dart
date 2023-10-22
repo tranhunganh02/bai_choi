631dc1cd17e77ef3c7770522e1a44b95a1040cbd
tranhunganh02 <hunganh.dh2021@gmail.com>
2023-10-22 22:23:20 +0700

    Update introduce and tutorial

--- lib/screens/introduction_game/components/TutorialScreen.dart
+++ lib/screens/introduction_game/components/TutorialScreen.dart
@@ -18,16 +18,13 @@ Container TutorialScreen(BuildContext context) {
                       fontSize: 22,
                       fontFamily: 'DelaGothicOne'),
                   child: const Text(
-                    "Giới thiệu về bài chòi \n",
+                    "Cách chơi \n",
                     textAlign: TextAlign.center,
                   ))),
           content(
               "Là ứng dụng offline 4 người chơi trên một máy ngồi xung quanh với nhau (tương tự như các chòi đặt xung quanh anh Hiệu).\n"),
-          content(
-              "Các lá bài trong game có 3 pho bài, mỗi pho 10 quân, tổng cộng 30 lá bài. \n"),
-          content(
-              "Sẽ có 2 mode chơi: mode chơi bình thường và mode chơi nhanh. Ở mode chơi bình thường: Anh Hiệu sẽ hò tối đa 9 quân bài.Sau khi hò xong, ai có số lượng bài quân trúng nhiều hơn sẽ dành chiến thắng, trong trường hợp 2 người có số lượng bài quân bằng nhau thì sẽ xử hòa. Ở mode chơi bình thường: Anh Hiệu sẽ hò cho đến khi tìm ra được người chiến thắng (trúng 3 quân bài)."),
-
+          content("Các lá bài trong game có 3 pho bài, mỗi pho 10 quân, tổng cộng 30 lá bài. Trước khi bắt đầu, nếu người chơi là lần đầu tham gia, người chơi sẽ nhập tên cho 4 người hoặc chọn tên mặc định. Nếu đã từng chơi, tên người dùng đã lưu từ lần chơi cuối sẽ được sử dụng. Người dùng có thể thay đổi tên ở phần cài đặt ngoài Main Menu.\n"),
+          content("Sau khi nhập tên, người chơi sẽ vào phần chọn mode chơi. Sẽ có hai mode chơi: mode chơi bình thường và mode chơi nhanh. Ở mode chơi nhanh: ứng dụng phát ngẫu nhiên tối đa 9 câu hô thai của anh Hiệu, chị Hiệu, người chơi có số lượng lá bài trúng nhiều hơn sẽ dành chiến thắng, trong trường hợp 2 người có số lượng lá bài trúng bài bằng nhau thì sẽ xử hòa. Ở mode chơi bình thường: ứng dụng phát ngẫu nhiên câu hô của anh Hiệu, chị Hiệu cho đến khi tìm ra được người chiến thắng là người chơi trúng 3 quân bài. \nVào lượt chơi, mỗi người sẽ được phát ngẫu nhiên 3 lá bài, mỗi lá từ một pho khác nhau. Lượt chơi sẽ bắt đầu phát đoạn intro, đoạn này từ 10-15s tạo sự hào hứng cho người chơi.\n\n Với sự cho phép của nghệ sỹ Huyền Tân và các nghệ sỹ tròn câu lạc bộ bài chòi Sông Hàn, chúng em sử dụng các video hô hát bài chòi trên kênh youtube Huyền Tân Bài Chòi, random ngẫu nhiên các câu hô thai, nếu câu hô thai nào trúng với lá bài người chơi đang có thì người chơi được nhận một cờ xanh, nếu người chơi đủ 3 cờ xanh là người chiến thắng và kết thúc lượt chơi. Ngoài ra, người dùng còn có thể tìm hiểu thêm về nguồn gốc ra đời của bài chòi, tìm hiểu về 30 lá bài và các câu hô thai tương ứng. mỗi bài sẽ kèm theo một nút skip để người chơi có thể tua về gần cuối video, lúc anh Hiệu, chị Hiệu đọc tên lá bài. "),
         ],
       ),
     ),
--- lib/screens/introduction_game/components/introduce_screen.dart
+++ lib/screens/introduction_game/components/introduce_screen.dart
@@ -20,23 +20,56 @@ Container IntroduceScreen(BuildContext context) {
                     textAlign: TextAlign.center,
                   ))),
           content(
-              "Nghệ thuật Bài Chòi Trung Bộ của Việt Nam được UNESCO công nhận là di sản văn hóa phi vật thể đại diện của nhân loại từ năm 2017. Bài Chòi Trung bộ Việt Nam xuất hiện từ cách đây hàng trăm năm. Nguồn gốc của Bài Chòi được cho là bắt nguồn từ công cuộc khẩn hoang. Người dân khi ấy dựng chòi trên những khu đất rừng mới vỡ để canh chừng thú dữ, bảo vệ hoa màu. Trong chòi có để các nhạc cụ như mõ, canh, trống nhằm tạo tiếng động xua thú dữ. Và nghệ thuật bài chòi đã ra đời như một thú vui lúc nhàn rỗi của người dân ở đây. Ngày nay, Bài Chòi vẫn được thịnh hành ở các tỉnh thành khắp miền Trung từ Quảng Bình đến Bình Định.\n"),
+              "Ngày 7/12/2017, tại phiên họp Ủy ban liên Chính phủ Công ước 2003 về bảo vệ di sản văn hóa phi vật thể lần thứ 12 của Tổ chức Giáo dục, Khoa học và Văn hóa của LHQ (UNESCO) diễn ra tại Hàn Quốc và “Nghệ thuật Bài Chòi Trung bộ Việt Nam” đã được UNESCO ghi danh trong danh sách “Di sản Văn hóa phi vật thể đại diện của nhân loại”. Đây được xem là niềm vui to lớn đôi với nhân dân các tỉnh Trung Bộ nói riêng, cũng như nhân dân cả nước nói chung."),
           content(
-              "Chơi Bài Chòi đơn giản là một kiểu ngồi trên chòi đánh bài. Nhưng không chỉ là một trò chơi mà Bài Chòi là cả một nghệ thuật diễn xướng hát / hô của các nghệ nhân chính trong vai trò \"anh hiệu\" (những người quản trò)."),
+              "Bài chòi là một loại hình dân ca và trò chơi dân gian đặc trưng ở miền Trung Việt Nam, về sau đã được phát triển thành một loại hình sân khấu ca kịch. Bài chòi là di sản văn hóa của miền Trung mà nhân dân ở miền đất này đã nuôi dưỡng nó, bảo vệ nó từ xa xưa cho đến hôm nay, đây được xem là nghệ thuật mang rõ nét dân gian đặc sắc nhất, không lai tạp trong âm nhạc, trong ca hát và trong diễn xuất, là sản phẩm tinh thần hồn nhiên được sinh ra từ những con người hồn nhiên, chất phát, nó do chính người lao động ở nông thôn sáng tạo ra, và nó đã trở thành món ăn tinh thần không thể thiếu của họ."),
           Title(
               color: Theme.of(context).colorScheme.inversePrimary,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 32.0),
-                child: DefaultTextStyle(
-                    style: TextStyle(
-                        color: lightColorScheme.onSecondary,
-                        fontSize: 22,
-                        fontFamily: 'DelaGothicOne'),
-                    child: const Text(
-                      "Về trò chơi \n",
-                      textAlign: TextAlign.center,
-                    )),
-              )),
+              child: DefaultTextStyle(
+                  style: TextStyle(
+                      color: lightColorScheme.onSecondary,
+                      fontSize: 19,
+                      fontFamily: 'DelaGothicOne'),
+                  child: const Text(
+                    " Nguồn gốc xuất xứ Bài Chòi \n",
+                    textAlign: TextAlign.left,
+                  ))),
+
+          content("Hội Bài Chòi là một trong những tổ chức vui xuân của nhân dân, xuất xứ từ Bình Định rồi sau mới lan các tỉnh lân cận như Quảng Ngãi, Quảng Nam và vào đến Phú Yên, Khánh Hòa, Bình Thuận. Có thể nói nguồn gốc, thời gian xuất hiện chính xác của Bài Chòi đang là những câu hỏi còn gây nhiều tranh cãi.\n"),
+          content("Theo lời cụ Phan Đình Lang[ Tức Bốn Trang, còn gọi là Bốn Que, một lão nghệ nhân Bài Chòi, sinh năm 1910, ở xã Nhơn Thành, huyện An Nhơn, tỉnh Bình Định] thì chính cụ đã từng nghe ông nội, ông thân cụ và cùng nhiều người khác kể lại là hội Bài Chòi là do ông Đào Duy Từ (1571-1634) sáng lập, ông Đào Duy Từ từ Bắc vào đến Bình Định thì lại lo việc di dân lập ấp. Trong thời kỳ đầu, việc khai khẩn đất hoang được coi là việc chủ yếu. Trên từng khu đất mới vỡ và bắt đầu sản xuất, người ta phải cất những cái Chòi cao làm bằng tranh, bằng tre để canh giữ thú rừng, bảo vệ hoa màu. Trên mỗi Chòi đều có mõ, thanh la, trống hoặc các thứ để gây tiếng động, để mỗi khi có thú rừng kéo đến phá hoại hoa màu hay uy hiếp người canh giữ thì lập tức các thứ dùng để làm thanh viện đó đồng khua ầm lên để xua đuổi thú dữ. Từ việc tổ chức các Chòi canh để bảo vệ sản xuất, đến việc các nghệ nhân mượn cuộc chơi bài tứ sắc vận vào câu hô, ông Đào Duy Từ mới nghĩ ra một trò chơi trong dịp Tết: Hội Bài Chòi.\n"),
+          content("Theo đề tài nghiên cứu cấp Bộ về “Bảo tồn và phát huy các giá trị của nghệ thuật Bài Chòi trong đời sống hôm nay (11/12/2014) của GS. Hoàng Chương cho rằng “Trò chơi nghệ thuật độc đáo này ra đời khoảng cuối TK19 đầu TK20 và tồn tại cho đến hôm nay, chỉ gián đoạn trong thời chiến tranh chống Pháp và chống Mỹ, nhưng lại chuyển sang hình thức hô, hát Bài chòi rất phổ biến trong nhân dân và trong quân đội...” \n"),
+          content("Nhìn lại chặng đường lịch sử Bài Chòi, có thể chia ra làm bốn giai đoạn:"),
+          content("Giai đoạn thứ nhất: điệu hát sinh ra từ hội chòi và hoạt động phục vụ hội chòi để  phát triển và hoàn chỉnh thành điệu Bài Chòi."),
+          content("Giai đoạn thứ hai: Bài Chòi vượt ra ngoài phạm vi hội chòi biểu diễn trên “sân khấu trải chiếu” dẫn đến sự kiện “từ đất lên giàn” và các gánh bài chòi nổi lên hoạt động mạnh mẽ cho đến Cách mạng Tháng Tám"),
+          content("Giai đoạn thứ ba: Bài Chòi phục vụ đắc lực cho công cuộc kháng chiến chống Pháp của nhân dân khu Nam"),
+          content("Giai đoạn thứ tư: Bài Chòi được xây dựng thành một bộ môn nghệ thuật ca kịch dân tộc hiện đại."),
+          
+           Title(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              child: DefaultTextStyle(
+                  style: TextStyle(
+                      color: lightColorScheme.onSecondary,
+                      fontSize: 19,
+                      fontFamily: 'DelaGothicOne'),
+                  child: const Text(
+                    " Sự ra đời của làn điệu Bài Chòi \n",
+                    textAlign: TextAlign.left,
+                  ))),
+          
+          content("Làn điệu Bài Chòi được viết ra trên 4 âm điệu Bài Chòi cơ bản là: Xuân nữ, Xàng xê, Nam Xuân (Cổ bản), Hồ quảng. Về lời văn thì theo các thể thơ: lục bát, song thất lục bát, lục bát biến thể, thơ tứ ngôn, ngũ ngôn, thất ngôn tứ tuyệt, biền văn (văn biền ngẫu) và văn vần chiếm ưu thế tuyệt đối."),
+          content("Bài Chòi thuộc thể loại âm nhạc kể chuyện, khúc thức tự do, không cố định. Bài chòi mang một loại hình tiết tấu riêng biệt, không giống với loại hình nghệ thuật nào khác. Cùng một đoạn nhạc Bài Chòi có nội dung và làn điệu như nhau, nhưng giữa người này và người kia có cách nghiên cứu khác nhau cũng sẽ dẫn tới nhiều cách hát, và đây chính là nét đặc trưng của Bài chòi với phong cách kể diễn."),
+
+          Title(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              child: DefaultTextStyle(
+                  style: TextStyle(
+                      color: lightColorScheme.onSecondary,
+                      fontSize: 19,
+                      fontFamily: 'DelaGothicOne'),
+                  child: const Text(
+                    "Về bản quyền hình ảnh và video \n",
+                    textAlign: TextAlign.left,
+                  ))),
           content(
               "Xin cảm ơn nghệ sỹ Huyền Tân và các nghệ sỹ trong CLB Bài chòi Sông Hàn đã hỗ trợ cố vấn. Trò chơi có sử dụng video của các cô chú để phục vụ trò chơi."),
           content(
@@ -50,6 +83,6 @@ Container IntroduceScreen(BuildContext context) {
 DefaultTextStyle content(String text) {
   return DefaultTextStyle(
     style: TextStyle(color: lightColorScheme.onPrimary, fontSize: 12, fontFamily: "DelaGothicOne"),
-    child: Text(text),
+    child: Text("$text\n"),
   );
 }
